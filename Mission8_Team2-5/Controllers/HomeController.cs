e3c54af74e26a49e54ef0a2e02ff835c600a13d6
bwearly <153124661+bwearly@users.noreply.github.com>
2025-02-28 12:23:38 -0700

    added CRUD functionality to the controller

--- Mission8_Team2-5/Controllers/HomeController.cs
+++ Mission8_Team2-5/Controllers/HomeController.cs
@@ -13,24 +13,68 @@ namespace Mission8_Team2_5.Controllers
             _context = temp;
         }
 
-        public HomeController(ILogger<HomeController> logger) => _logger = logger;
-
         public IActionResult Index()
         {
 
             return View();
         }
 
+        [HttpGet]
         public IActionResult TaskForm()
         {
-            ViewBag.Categories = _context.Categories;
+            ViewBag.Categories = _context.Category.ToList();
 
-            return View();
+            return View("TaskForm", new Task());
         }
 
-        public IActionResult TaskForm()
+        [HttpPost]
+        public IActionResult TaskForm(Task reponse)
         {
-            return Redirect("Delete");
+            if (ModelState.IsValid)
+            {
+                _context.Task.Add(response);
+                _context.SaveChanges();
+
+                return View("Confirmation", reponse);
+            }
+            else
+            {
+                ViewBag.Categories = _context.Category.ToList();
+                return View(response);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult EditTask(int id)
+        {
+            ViewBag.Categories = _context.Category.ToList();
+            var taskToEdit = _context.Task.Single(x => x.TaskId == id);
+
+            return View("TaskForm", taskToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult EditTask(Task editTask)
+        {
+            _context.Update(editTask);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteTask(int id)
+        {
+            var recordToDelete = _context.Movies.Single(x => x.TaskId == id);
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTask(Task deleteTask)
+        {
+            _context.Task.Remove(deleteTask);
+            _context.SaveChanges();
+            return RedirectToAction("Delete");
         }
     }
 }
