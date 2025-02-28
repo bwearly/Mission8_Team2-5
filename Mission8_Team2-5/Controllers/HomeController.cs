42acd11615cbd02d50bf0554dd9e58890fcbaf50
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-02-28 13:17:19 -0700

    kjfrk

--- Mission8_Team2-5/Controllers/HomeController.cs
+++ Mission8_Team2-5/Controllers/HomeController.cs
@@ -7,74 +7,74 @@ namespace Mission8_Team2_5.Controllers
 {
     public class HomeController : Controller
     {
-        private TimeManageMatrixContext _context;
-        public HomeController(TimeManageMatrixContext temp)
-        {
-            _context = temp;
-        }
+        //private TimeManageMatrixContext _context;
+        //public HomeController(TimeManageMatrixContext temp)
+        //{
+        //    _context = temp;
+        //}
 
-        public IActionResult Index()
-        {
+        //public IActionResult Index()
+        //{
 
-            return View();
-        }
+    //        return View();
+    //    }
 
-        [HttpGet]
-        public IActionResult TaskForm()
-        {
-            ViewBag.Categories = _context.Category.ToList();
+    //    [HttpGet]
+    //    public IActionResult TaskForm()
+    //    {
+    //        ViewBag.Categories = _context.Category.ToList();
 
-            return View("TaskForm", new Task());
-        }
+    //        return View("TaskForm", new Task());
+    //    }
 
-        [HttpPost]
-        public IActionResult TaskForm(Task reponse)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Task.Add(response);
-                _context.SaveChanges();
+    //    [HttpPost]
+    //    public IActionResult TaskForm(Task reponse)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            _context.Task.Add(response);
+    //            _context.SaveChanges();
 
-                return View("Confirmation", reponse);
-            }
-            else
-            {
-                ViewBag.Categories = _context.Category.ToList();
-                return View(response);
-            }
-        }
+    //            return View("Confirmation", reponse);
+    //        }
+    //        else
+    //        {
+    //            ViewBag.Categories = _context.Category.ToList();
+    //            return View(response);
+    //        }
+    //    }
 
-        [HttpGet]
-        public IActionResult EditTask(int id)
-        {
-            ViewBag.Categories = _context.Category.ToList();
-            var taskToEdit = _context.Task.Single(x => x.TaskId == id);
+    //    [HttpGet]
+    //    public IActionResult EditTask(int id)
+    //    {
+    //        ViewBag.Categories = _context.Category.ToList();
+    //        var taskToEdit = _context.Task.Single(x => x.TaskId == id);
 
-            return View("TaskForm", taskToEdit);
-        }
+    //        return View("TaskForm", taskToEdit);
+    //    }
 
-        [HttpPost]
-        public IActionResult EditTask(Task editTask)
-        {
-            _context.Update(editTask);
-            _context.SaveChanges();
+    //    [HttpPost]
+    //    public IActionResult EditTask(Task editTask)
+    //    {
+    //        _context.Update(editTask);
+    //        _context.SaveChanges();
 
-            return RedirectToAction("Index");
-        }
+    //        return RedirectToAction("Index");
+    //    }
 
-        [HttpGet]
-        public IActionResult DeleteTask(int id)
-        {
-            var recordToDelete = _context.Movies.Single(x => x.TaskId == id);
-            return View(recordToDelete);
-        }
+    //    [HttpGet]
+    //    public IActionResult DeleteTask(int id)
+    //    {
+    //        var recordToDelete = _context.Movies.Single(x => x.TaskId == id);
+    //        return View(recordToDelete);
+    //    }
 
-        [HttpPost]
-        public IActionResult DeleteTask(Task deleteTask)
-        {
-            _context.Task.Remove(deleteTask);
-            _context.SaveChanges();
-            return RedirectToAction("Delete");
-        }
+    //    [HttpPost]
+    //    public IActionResult DeleteTask(Task deleteTask)
+    //    {
+    //        _context.Task.Remove(deleteTask);
+    //        _context.SaveChanges();
+    //        return RedirectToAction("Delete");
+    //    }
     }
 }
